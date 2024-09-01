7ea9ae6a66a43ac1eb7bb8a9e14a24a38e806979
Ketan Patil <ketankp06@gmail.com>
2024-09-02 00:28:57 +0530

    Implementing Header part

--- swiggy-clone/src/app/app.module.ts
+++ swiggy-clone/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainHeaderComponent } from './component/main-header/main-header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
